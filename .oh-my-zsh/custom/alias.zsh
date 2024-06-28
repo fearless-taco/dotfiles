127767c4072b796f450f6cca0dcfe992d07885d0
Jason Aagaard <jason.aagaard@icloud.com>
2024-06-28 15:28:07 +0200

    Add alias to delete derived data

--- .oh-my-zsh/custom/alias.zsh
+++ .oh-my-zsh/custom/alias.zsh
@@ -1,3 +1,6 @@
 # Dotfiles Bare Repository
 alias dot='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
+# Delete DerivedData
+alias xplode='rm -rf ~/Library/Developer/Xcode/DerivedData'
+
